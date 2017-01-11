f09b55d04ee7185410a82c9ee808d9018a0b4ae4
SunniyaMaini <sunniya.maini@tothenew.com>
2017-01-11 14:34:25 +0530

    added code for first session

--- 1-datatypes.js
+++ 1-datatypes.js
@@ -1,5 +1,7 @@
-// code 1 - rgb to hex
+// data type of null is object:
+null and undefined are both false for value equality (null==undefined): they both collapse to boolean false. They are not the same object (null!==undefined). undefined is a property of the global object ("window" in browsers), but is a primitive type and not an object itself.
 
+// code 1 - rgb to hex
 function toHex(r,g,b) {
 	return "#" + r.toString(16) + g.toString(16) + b.toString(16);
 }
