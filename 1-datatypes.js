dab67fd02a2b211c7eb7d69870853eacf260d4d7
SunniyaMaini <sunniya.maini@tothenew.com>
2017-01-11 16:05:36 +0530

    added code for first session

--- 1-datatypes.js
+++ 1-datatypes.js
@@ -1,6 +1,12 @@
 // data type of null is object:
 null and undefined are both false for value equality (null==undefined): they both collapse to boolean false. They are not the same object (null!==undefined). undefined is a property of the global object ("window" in browsers), but is a primitive type and not an object itself.
 
+// custom function to get datatype of any passed parameter or value
+function getType(x) {
+	return "DataType of this data is:" + typeof(x);
+}
+getType("ABC");
+
 // code 1 - rgb to hex
 function toHex(r,g,b) {
 	return "#" + r.toString(16) + g.toString(16) + b.toString(16);
